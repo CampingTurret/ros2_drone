1a626da24bc296a8e568d9de698a1e9303690180
CampingTurret <lorenzo.gaeta@hotmail.nl>
2026-04-17 02:43:35 +0200

    infinite sleep

--- run_script.py
+++ run_script.py
@@ -66,7 +66,8 @@ def start_commander_container():
         "--device", "/dev/gpiomem4",
         "--device", "/dev/mem",
         "-v", f"{PATH_Commander}/logs:/root/logs",
-        "elijahanghw/ros_imav:latest"
+        "elijahanghw/ros_imav:latest",
+        "sleep", "infinity"
     ]
 
     # Start container
